32d9eb89d440923add6d1b49aa6fd94a4ac2c3b3
Ayush_sccg <ayush1240945@gmail.com>
2024-01-31 14:27:00 +0545

    routes

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,5 +1,6 @@
 import express from "express";
+import { newUser } from "../controllers/user.js";
 const app = express.Router();
 // /api/v1/user/new
-app.post("/new", (req, res, next) => { });
+app.post("/new", newUser);
 export default app;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { newUser } from "../controllers/user.js";
 
 const app = express.Router();
 
 // /api/v1/user/new
-app.post("/new" , (req , res , next)=>{})
+app.post("/new", newUser);
 
 export default app;
