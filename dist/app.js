38861746051444c50596e8a5a11d1966117da999
Ayush_sccg <ayush1240945@gmail.com>
2024-01-31 15:10:45 +0545

    .

--- dist/app.js
+++ dist/app.js
@@ -5,11 +5,18 @@ import { connectDB } from "./utils/features.js";
 const app = express();
 const port = 3000;
 connectDB();
+// middleware
 app.use(express.json());
 // using routers
 app.use("/api/v1/user", userRoutes);
 app.get("/", (req, res) => {
     res.send("working");
+});
+app.use((err, req, res, next) => {
+    return res.status(400).json({
+        success: true,
+        message: "some erroer"
+    });
 });
 app.listen(port, () => {
     console.log(`Server is working on http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction } from "express";
+import { Request, Response  } from "express";
 
 // importing routes
 import userRoutes from "./routes/user.js";
@@ -7,6 +8,7 @@ const app = express();
 const port: number = 3000;
 
 connectDB();
+// middleware
 app.use(express.json());
 // using routers
 app.use("/api/v1/user", userRoutes);
@@ -14,6 +16,13 @@ app.use("/api/v1/user", userRoutes);
 app.get("/", (req, res) => {
   res.send("working");
 });
+
+app.use((err:Error , req:Request , res:Response , next:NextFunction)=>{
+    return res.status(400).json({
+        success:true,
+        message:"some erroer"
+    })
+})
 
 app.listen(port, () => {
   console.log(`Server is working on http://localhost:${port}`);
