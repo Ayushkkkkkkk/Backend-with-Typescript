c5f85ef64e9ab72bda5648469c67c313f52d23d3
Ayush_sccg <ayush1240945@gmail.com>
2024-01-31 14:41:56 +0545

    database connection

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import express from "express";
 
 // importing routes
 import userRoutes from "./routes/user.js";
+import { connectDB } from "./utils/features.js";
 const app = express();
 const port: number = 3000;
 
+connectDB();
+
 // using routers
 app.use("/api/v1/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
 app.listen(port, () => {
   console.log(`Server is working on http://localhost:${port}`);
