bc31b9776ae04aedd5f927c471acfa0b9a59132a
Ayush_sccg <ayush1240945@gmail.com>
2024-01-31 14:23:17 +0545

    newUserRequestBody type

--- dist/controllers/user.js
+++ dist/controllers/user.js
@@ -1,2 +1,14 @@
-export const newUser = (req, res, next) => {
+import { User } from "../models/user.js";
+export const newUser = async (req, res, next) => {
+    try {
+        const { name, email, photo, gender, role, _id, dob } = req.body;
+        const user = await User.create({
+            name, email, photo, gender, role, _id, dob
+        });
+        return res.status(300).json({
+            sucess: true,
+            message: `welcome , ${user.name}`,
+        });
+    }
+    catch (error) { }
 };
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,22 @@
-import {Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
 
-export const newUser = (req:Request , res:Response , next:NextFunction)=>{
+export const newUser = async (
+  req: Request<{} , {} , NewUserRequestBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {name , email , photo , gender , role , _id , dob} = req.body;
 
-}
+    const user = await User.create({
+        name , email , photo , gender , role , _id , dob
+    });
+
+    return res.status(300).json({
+      sucess: true,
+      message: `welcome , ${user.name}`,
+    });
+  } catch (error) {}
+};
