841bb147c18a1f385e7eb567d907e086ff1b5c22
Ayush_sccg <ayush1240945@gmail.com>
2024-02-02 17:29:38 +0545

    order completed

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -234,36 +234,4 @@ export const getAllProducts = ProductTryCatch(
   }
 );
 
-const generateRandomProducts = async (count: number = 10) => {
-  const products = [];
 
-  for (let i = 0; i < count; i++) {
-    const product = {
-      name: faker.commerce.productName(),
-      photo: "uploads\\5ba9bd91-b89c-40c2-bb8a-66703408f986.png",
-      price: faker.commerce.price({ min: 1500, max: 80000, dec: 0 }),
-      stock: faker.commerce.price({ min: 0, max: 100, dec: 0 }),
-      category: faker.commerce.department(),
-      createdAt: new Date(faker.date.past()),
-      updatedAt: new Date(faker.date.recent()),
-      __v: 0,
-    };
-
-    products.push(product);
-  }
-
-  await Product.create(products);
-
-  console.log({ succecss: true });
-};
-
-const deleteRandomsProducts = async (count: number = 10) => {
-  const products = await Product.find({}).skip(2);
-
-  for (let i = 0; i < products.length; i++) {
-    const product = products[i];
-    await product.deleteOne();
-  }
-
-  console.log({ succecss: true });
-};
