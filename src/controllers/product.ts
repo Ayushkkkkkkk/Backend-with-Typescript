6b93152542d2535c26350dff08d3ec37edd2de61
Ayush_sccg <ayush1240945@gmail.com>
2024-02-02 17:25:44 +0545

    generate random product

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -196,9 +196,6 @@ export const getAllProducts = ProductTryCatch(
   ) => {
     const { search, sort, category, price } = req.query;
     const page = Number(req.query.page) || 1;
-    // 1,2,3,4,5,6,7,8
-    // 9,10,11,12,13,14,15,16
-    // 17,18,19,20,21,22,23,24
     const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
     const skip = (page - 1) * limit;
 
@@ -236,3 +233,37 @@ export const getAllProducts = ProductTryCatch(
     });
   }
 );
+
+const generateRandomProducts = async (count: number = 10) => {
+  const products = [];
+
+  for (let i = 0; i < count; i++) {
+    const product = {
+      name: faker.commerce.productName(),
+      photo: "uploads\\5ba9bd91-b89c-40c2-bb8a-66703408f986.png",
+      price: faker.commerce.price({ min: 1500, max: 80000, dec: 0 }),
+      stock: faker.commerce.price({ min: 0, max: 100, dec: 0 }),
+      category: faker.commerce.department(),
+      createdAt: new Date(faker.date.past()),
+      updatedAt: new Date(faker.date.recent()),
+      __v: 0,
+    };
+
+    products.push(product);
+  }
+
+  await Product.create(products);
+
+  console.log({ succecss: true });
+};
+
+const deleteRandomsProducts = async (count: number = 10) => {
+  const products = await Product.find({}).skip(2);
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    await product.deleteOne();
+  }
+
+  console.log({ succecss: true });
+};
